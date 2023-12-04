5fde4d54d42bc12eb9be94798317ff545e5a508b
Niaz Rahman <76751075+niazrahman13@users.noreply.github.com>
2023-12-05 03:03:57 +0600

    Add files via upload

---
+++ dist/src/app.js
@@ -0,0 +1,18 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const body_parser_1 = __importDefault(require("body-parser"));
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const users_router_1 = require("./user/users.router");
+const app = (0, express_1.default)();
+app.use(body_parser_1.default.json());
+app.use((0, cors_1.default)());
+app.get('/', (req, res) => {
+    res.send('Hello');
+});
+// application routes
+app.use('/api/user', users_router_1.userRouter);
+exports.default = app;
